bcc9828bca1d5921ad8568a20cc442fd11191781
pierre collinet <pi.collinet@gmail.com>
2018-03-27 14:09:20 +0200

    ...

--- blablacar1/urls.py
+++ blablacar1/urls.py
@@ -41,8 +41,7 @@ urlpatterns = [
     
     url(r'^admin/', include(admin.site.urls)),
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+] 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
