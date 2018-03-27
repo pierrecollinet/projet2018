9571a50067d8af2a37d8e6d5b5af98dddf5148b0
pierre collinet <pi.collinet@gmail.com>
2018-03-27 14:15:34 +0200

    ...

--- blablacar1/settings/production.py
+++ blablacar1/settings/production.py
@@ -117,9 +117,10 @@ DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = "app-root/repo/wsgi/static"
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = STATIC_URL = '/static/'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
